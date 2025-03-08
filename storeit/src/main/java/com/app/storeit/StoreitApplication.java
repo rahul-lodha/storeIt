f97280611b834a001700f7a895f11427dc9da893
rahul <rahullodha2001@gmail.com>
2025-03-08 19:00:24 +0530

    spring init

---
+++ storeit/src/main/java/com/app/storeit/StoreitApplication.java
@@ -0,0 +1,13 @@
+package com.app.storeit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreitApplication.class, args);
+	}
+
+}
---
+++ storeit/src/test/java/com/app/storeit/StoreitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.storeit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
